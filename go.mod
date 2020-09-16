e2bf6ce887e6b4909c63cfa8fc4e9072253f8efd
Alejandro Valdevila Pérez <avaldevilap@gmail.com>
2020-09-16 12:36:34 -0400

    Added all command to CLI

--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/avaldevilap/tasker
 go 1.15
 
 require (
-	github.com/urfave/cli/v2 v2.2.0 // indirect
-	go.mongodb.org/mongo-driver v1.4.1 // indirect
+	github.com/urfave/cli/v2 v2.2.0
+	go.mongodb.org/mongo-driver v1.4.1
 	golang.org/x/tools/gopls v0.5.0 // indirect
+	gopkg.in/gookit/color.v1 v1.1.6
 )
--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"context"
 	"errors"
+	"fmt"
 	"github.com/urfave/cli/v2"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"gopkg.in/gookit/color.v1"
 	"log"
 	"os"
 	"time"
@@ -64,6 +67,25 @@ func main() {
 					return createTask(task)
 				},
 			},
+			{
+				Name:    "all",
+				Aliases: []string{"l"},
+				Usage:   "list all tasks",
+				Action: func(c *cli.Context) error {
+					tasks, err := getAll()
+					if err != nil {
+						if err == mongo.ErrNoDocuments {
+							fmt.Print("Nothing to see here.\nRun `add 'task'` to add a task")
+							return nil
+						}
+
+						return err
+					}
+
+					printTasks(tasks)
+					return nil
+				},
+			},
 		},
 	}
 
@@ -73,7 +95,53 @@ func main() {
 	}
 }
 
+func printTasks(tasks []*Task) {
+	for i, v := range tasks {
+		if v.Completed {
+			color.Green.Printf("%d: %s\n", i+1, v.Text)
+		} else {
+			color.Yellow.Printf("%d: %s\n", i+1, v.Text)
+		}
+	}
+}
+
 func createTask(task *Task) error {
 	_, err := collection.InsertOne(ctx, task)
 	return err
+}
+
+func getAll() ([]*Task, error) {
+	filter := bson.D{{}}
+	return filterTasks(filter)
+}
+
+func filterTasks(filter interface{}) ([]*Task, error) {
+	var tasks []*Task
+
+	cur, err := collection.Find(ctx, filter)
+	if err != nil {
+		return tasks, err
+	}
+
+	for cur.Next(ctx) {
+		var t Task
+		err := cur.Decode(&t)
+		if err != nil {
+			return tasks, err
+		}
+
+		tasks = append(tasks, &t)
+	}
+
+	if err := cur.Err(); err != nil {
+		return tasks, err
+	}
+
+	cur.Close(ctx)
+
+	if len(tasks) == 0 {
+		return tasks, mongo.ErrNoDocuments
+	}
+
+	return tasks, nil
 }
